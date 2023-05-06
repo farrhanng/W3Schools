f83e157520c4620b5435f70a6e3c33d57d5c8173
Farrhan <103014061+farrhannyong@users.noreply.github.com>
2023-05-06 08:34:52 +0000

    end of references

--- JSTutorial/Objects.js
+++ JSTutorial/Objects.js
@@ -1,3 +1,18 @@
-const Vanessa = {firstname: "Vanessa", lastname: "Ng", age: 27, weight: 70, fullname: function(){return this.firstname + " " + this.lastname}};
+const Vanessa = {
+    firstname: "Vanessa",
+    lastname: "Ng",
+    age: 27,
+    weight: 70,
+    fullname: function() {
+        return this.firstname + " " + this.lastname;
+    }
+};
 
-console.log(this.fullname + " is " + Vanessa.age + " years old.");
+console.log(Vanessa.fullname() + " is " + Vanessa.age + " years old.");
+
+const person = {
+    firstName: "John",
+    lastName: "Doe"
+};
+
+console.log(person.firstName);
